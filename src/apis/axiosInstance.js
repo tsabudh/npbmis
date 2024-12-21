c67ca1545da8f427f913ecd334d348aab473eb47
tsabudh <tsabudh@gmail.com>
2024-12-21 13:50:52 +0545

    chore: changed URI

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const API_URI =
-  process.env.NODE_ENV === "production"
-    ? "https://npbmis-server.vercel.app/api/v1"
-    : "http://localhost:3000/api/v1";
+const API_URI = process.env.API_URI || "http://localhost:3000/api/v1";
 //
 const axiosInstance = axios.create({
   baseURL: API_URI,
